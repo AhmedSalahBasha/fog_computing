4cb545b6c16ce7c5fc3668aaf52b70d662d3a318
AhmedSalahBasha <a.salah.b@gmail.com>
2019-06-10 14:15:20 +0200

    add prediction message

--- cloud/receive_reply.py
+++ cloud/receive_reply.py
@@ -9,7 +9,51 @@ Created on Sat Jun  8 23:56:22 2019
 
 import socket    # Import socket module
 import time
+from statistics import mean
+from decimal import Decimal
 
+
+def get_air_pressure_stats(listofvalues):
+    air_pressure_list = []
+    for val in listofvalues:
+        air_pressure_list.append(float(val[1]))
+    mean_val = round(Decimal(mean(air_pressure_list)), 3)
+    #stdev_val = round(Decimal(stdev(air_pressure_list)), 3)
+    #max_val = round(Decimal(max(air_pressure_list)), 3)
+    #min_val = round(Decimal(min(air_pressure_list)), 3)
+    return mean_val
+
+
+def create_prediction_msg(filename):
+    listofvalues = []
+    with open(filename, 'r') as file:
+        for line in file:
+            listofvalues.append(line.split('|'))
+    mean_val = get_air_pressure_stats(listofvalues)
+    
+    return_msg = 'Unknown Pressure Value!'
+    if mean_val >= 1086:
+        return_msg = 'Highest Ever Recorded'
+    elif mean_val >= 1030:
+        return_msg = 'Strong High Pressure System'
+    elif mean_val >= 1013:
+        return_msg = 'Average Sea Level Pressure'
+    elif mean_val >= 1000:
+        return_msg = 'Typical Low Pressure System'
+    elif mean_val >= 980:
+        return_msg = 'CAT 1 Hurricane or a very intense mid-latitude cyclone'
+    elif mean_val >= 950:
+        return_msg = 'CAT 3 Hurricane'
+    elif mean_val >= 980:
+        return_msg = 'Lowest Ever Recorded (not including tornadoes)'
+        
+    return return_msg
+
+
+def send_prediction_msg_to_local(filename):
+    return_msg = create_prediction_msg(filename)
+    conn.send(return_msg.encode())
+    print("Prediction message has been sent sussessfully!")
 
 keep_trying_to_connect = True
 
@@ -35,9 +79,14 @@ while keep_trying_to_connect:
         filename = 'air_pressure_data_' + str(int(time.time()))
         with open(filename, 'wb') as f:
             f.write(data)      # write data to a file
-            confirm_msg = 'MsgFromCloud:  File Has Been Received Succesfully!'
-            conn.send(confirm_msg.encode())
-            print('Successfully get the file')
+            #confirm_msg = 'MsgFromCloud:  File Has Been Received Succesfully!'
+            #conn.send(confirm_msg.encode())
+            
+        print('Successfully get the file')
+        return_msg = create_prediction_msg(filename)
+        conn.send(return_msg.encode())
+        print("Prediction message has been sent sussessfully!")
+        
 
 
 conn.close()
